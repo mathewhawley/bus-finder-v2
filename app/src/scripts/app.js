bd2e311ea567536209e64d9677dda9547900c121
Mathew Hawley <mathew@mathewhawley.com>
2015-07-13 22:12:58 +0100

    Begin map component

--- app/src/scripts/app.js
+++ app/src/scripts/app.js
@@ -2,15 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SiteHeader from './components/site-header';
 import Search from './components/search';
+import Map from './components/map';
 
 var mountNode = document.getElementById( 'app' );
 
 class App extends React.Component {
     render () {
         return (
-            <div>
+            <div className='site-wrapper'>
                 <SiteHeader />
                 <Search />
+                <Map />
             </div>
         );
     }
---
+++ app/src/scripts/components/map.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Map extends React.Component {
+    render () {
+        return (
+            <div className='map'></div>
+        );
+    }
+}
+
+export default Map;
