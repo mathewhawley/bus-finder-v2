28988fb85df02e6fee307d0c021dcc88b5f1e7d3
Mathew Hawley <mathew@mathewhawley.com>
2015-07-18 16:48:23 +0100

    Add check for geolocation functionality on button click

--- app/src/scripts/components/site-header/sh-geolocation.js
+++ app/src/scripts/components/site-header/sh-geolocation.js
@@ -2,7 +2,21 @@ import React from 'react';
 
 class Geolocation extends React.Component {
 
+    constructor( props ) {
+        super( props );
+        this.handleClick = this.handleClick.bind( this );
+        this.getLocation = this.getLocation.bind( this );
+    }
+
     handleClick() {
+        if ( 'geolocation' in navigator ) {
+            this.getLocation();
+        } else {
+            alert( 'Sorry, Geolocation seems to be unavailable, please update your browser settings' );
+        }
+    }
+
+    getLocation() {
         console.log( 'Working' );
     }
 
