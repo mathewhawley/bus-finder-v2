daf411aa02a0f0e714b3f961d9738f0879e341a5
Mathew Hawley <mathew@mathewhawley.com>
2015-07-23 23:25:20 +0100

    Clear markers before adding new ones

--- app/src/scripts/stores/OverlayStore.js
+++ app/src/scripts/stores/OverlayStore.js
@@ -2,14 +2,29 @@ var markers = [],
     placeIcon = '../../assets/marker-place.svg',
     busStopIcon = '../../assets/marker-bus-stop';
 
+function deleteMarkers() {
+    clearMarkers();
+    markers = [];
+}
+
+function clearMarkers() {
+    for ( let i = 0, l = markers.length; i < l; i++ ) {
+        markers[ i ].setMap( null );
+    }
+}
+
 var OverlayStore = {
 
     addPlaceMarker( map, position ) {
+
+        deleteMarkers();
+
         var marker = new google.maps.Marker( {
             position,
             map,
             icon: placeIcon
         } );
+
         markers.push( marker );
     }
 };
