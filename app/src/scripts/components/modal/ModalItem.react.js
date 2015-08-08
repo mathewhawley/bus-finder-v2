6a859fa9786dcffc7a798c9eada6fdb2daf1e33a
Mathew Hawley <mathew@mathewhawley.com>
2015-08-08 18:11:15 +0100

    Refine modal styling

--- app/src/scripts/components/modal/ModalItem.react.js
+++ app/src/scripts/components/modal/ModalItem.react.js
@@ -1,13 +1,30 @@
 import React from 'react';
 
+function sortWaitData( data ) {
+    if ( data === 'due' ) {
+        return <p>{ data.charAt( 0 ).toUpperCase() + data.slice( 1 ) }</p>;
+    } else {
+        let dataArray = data.split( ' ' );
+        return  <p>{ dataArray[0] }{ dataArray[1] }</p>;
+    }
+}
+
 class ModalItem extends React.Component {
 
     render() {
+
+        var waitData = this.props.estimatedWait,
+            estimatedWait = sortWaitData( waitData );
+
         return (
-            <div className={ this.props.className + '__item' } key={ this.props.key }>
-                <p>{ this.props.routeId }</p>
-                <p>{ this.props.estimatedWait }</p>
-                <p>{ this.props.destination }</p>
+            <div className={ this.props.className + '__item' }>
+                <div className={ this.props.className + '__item-route' }>
+                    <p>{ this.props.routeId }</p>
+                    <p>{ this.props.destination }</p>
+                </div>
+                <div className={ this.props.className + '__item-wait'}>
+                    { estimatedWait }
+                </div>
             </div>
         );
     }
